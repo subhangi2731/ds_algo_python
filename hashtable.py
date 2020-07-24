a25207f985fd6adeb8b499aed2e32e562849680c
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-24 21:28:00 +0530

    add hashtable

---
+++ hashtable.py
@@ -0,0 +1,14 @@
+class HashTable:
+    def __init__(self):
+        self.MAX = 100
+        self.arr = [None for i in range(self.MAX)]
+
+    def get_hash(self, key):
+        h = 0
+        for char in key:
+            h += ord(char)
+        return h % self.MAX
+
+
+t=HashTable()
+print(t.get_hash('apple'))
