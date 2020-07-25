b9193fb1f238da2cc9cf7c287c17a6c6c7a9590d
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-25 11:37:50 +0530

    get and delete

--- hashtable.py
+++ hashtable.py
@@ -1,7 +1,7 @@
 class HashTable:
     def __init__(self):
         self.MAX = 100
-        self.arr = [None for i in range(self.MAX)]
+        self.arr = [[] for i in range(self.MAX)]
 
     def get_hash(self, key):
         h = 0
@@ -11,18 +11,29 @@ class HashTable:
 
     def add(self,key,value):
         a = self.get_hash(key)
-        self.arr[a] = value
+        found = False
+        for i,el in enumerate(self.arr[a]):
+            if(len(el)==2 and el[0]==key):
+                self.arr[a][i]=(key,value)
+                found=True
+                break
+        if not found:
+            self.arr[a].append((key,value))
 
     def get_value(self,key):
         a = self.get_hash(key)
-        print(self.arr[a])
+        for el in self.arr[a]:
+            if(el[0]==key):
+                print(self.arr[a])
 
     def delete(self,key):
         a = self.get_value(key)
-        self.arr[a]=None
+        for i, el in enumerate(self.arr):
+            if(el[0]==key):
+                self.arr[a][i]=None
 
 t=HashTable()
-print(t.get_hash('apple'))
-t.add('march',28)
+t.add('march 6',28)
+t.add('march 6',34)
+t.add('march 17',11)
 print(t.arr)
-t.get_value('march')
